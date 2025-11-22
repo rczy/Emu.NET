461f295e64b2d6c2a66a5855a82f437f0379d147
rczy <berczy.balint@gmail.com>
2025-11-22 21:14:32 +0100

    Implemented read-modify-write operations.

---
+++ CPU.MOS6502.Tests/Unit/Instructions/Operations/ReadModifyWriteTests.cs
@@ -0,0 +1,118 @@
+namespace CPU.MOS6502.Tests.Unit.Instructions.Operations;
+
+public class ReadModifyWriteTests : Base
+{
+    private const ushort Address = 0x02CD;
+
+    [Theory]
+    [InlineData(0b0000_0000, 0b0000_0000, false, true, false)]
+    [InlineData(0b1000_0000, 0b0000_0000, false, true, true)]
+    [InlineData(0b0100_0000, 0b1000_0000, true, false, false)]
+    [InlineData(0b1100_0000, 0b1000_0000, true, false, true)]
+    [InlineData(0b0010_0000, 0b0100_0000, false, false, false)]
+    [InlineData(0b0000_0001, 0b0000_0010, false, false, false)]
+    [InlineData(0b1111_1111, 0b1111_1110, true, false, true)]
+    public void ASL_Executes_Correctly(byte input, byte result, bool negative, bool zero, bool carry)
+    {
+        system.CPU.Data = input;
+        system.CPU.Address.Full = Address;
+        Machinery.Instructions.ReadModifyWrite.Operations.ASL(system.CPU);
+        
+        Assert.Equal(Address, system.RAM.LastWriteAddress);
+        Assert.Equal(result, system.RAM.PeekAt(Address));
+        CheckFlags(negative, zero, carry, interrupt: false, @decimal: false, overflow: false);
+    }
+
+    [Theory]
+    [InlineData(0x02, 0x01, false, false)]
+    [InlineData(0x01, 0x00, false, true)]
+    [InlineData(0x00, 0xFF, true, false)]
+    [InlineData(0xFF, 0xFE, true, false)]
+    [InlineData(0x81, 0x80, true, false)]
+    [InlineData(0x80, 0x7F, false, false)]
+    public void DEC_Executes_Correctly(byte input, byte result, bool negative, bool zero)
+    {
+        system.CPU.Data = input;
+        system.CPU.Address.Full = Address;
+        Machinery.Instructions.ReadModifyWrite.Operations.DEC(system.CPU);
+        
+        Assert.Equal(Address, system.RAM.LastWriteAddress);
+        Assert.Equal(result, system.RAM.PeekAt(Address));
+        CheckFlags(negative, zero, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(0x01, 0x02, false, false)]
+    [InlineData(0x00, 0x01, false, false)]
+    [InlineData(0xFF, 0x00, false, true)]
+    [InlineData(0xFE, 0xFF, true, false)]
+    [InlineData(0x80, 0x81, true, false)]
+    [InlineData(0x7F, 0x80, true, false)]
+    public void INC_Executes_Correctly(byte input, byte result, bool negative, bool zero)
+    {
+        system.CPU.Data = input;
+        system.CPU.Address.Full = Address;
+        Machinery.Instructions.ReadModifyWrite.Operations.INC(system.CPU);
+        
+        Assert.Equal(Address, system.RAM.LastWriteAddress);
+        Assert.Equal(result, system.RAM.PeekAt(Address));
+        CheckFlags(negative, zero, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(0b0000_0000, 0b0000_0000, true, false)]
+    [InlineData(0b1000_0000, 0b0100_0000, false, false)]
+    [InlineData(0b0000_0010, 0b0000_0001, false, false)]
+    [InlineData(0b0000_0011, 0b0000_0001, false, true)]
+    [InlineData(0b0000_0100, 0b0000_0010, false, false)]
+    [InlineData(0b0000_0001, 0b0000_0000, true, true)]
+    [InlineData(0b1111_1111, 0b0111_1111, false, true)]
+    public void LSR_Executes_Correctly(byte input, byte result, bool zero, bool carry)
+    {
+        system.CPU.Data = input;
+        system.CPU.Address.Full = Address;
+        Machinery.Instructions.ReadModifyWrite.Operations.LSR(system.CPU);
+        
+        Assert.Equal(Address, system.RAM.LastWriteAddress);
+        Assert.Equal(result, system.RAM.PeekAt(Address));
+        CheckFlags(negative: false, zero, carry, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(0b0000_0000, 0b0000_0000, false, true, false, false)]
+    [InlineData(0b0000_0000, 0b0000_0001, false, false, true, false)]
+    [InlineData(0b1000_0000, 0b0000_0000, false, true, false, true)]
+    [InlineData(0b1000_0000, 0b0000_0001, false, false, true, true)]
+    [InlineData(0b1111_1111, 0b1111_1111, true, false, true, true)]
+    [InlineData(0b1111_1111, 0b1111_1110, true, false, false, true)]
+    public void ROL_Executes_Correctly(byte input, byte result, bool negative, bool zero, bool carryOld, bool carry)
+    {
+        system.CPU.Data = input;
+        system.CPU.Registers.P.Carry = carryOld;
+        system.CPU.Address.Full = Address;
+        Machinery.Instructions.ReadModifyWrite.Operations.ROL(system.CPU);
+        
+        Assert.Equal(Address, system.RAM.LastWriteAddress);
+        Assert.Equal(result, system.RAM.PeekAt(Address));
+        CheckFlags(negative, zero, carry, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(0b0000_0000, 0b0000_0000, false, true, false, false)]
+    [InlineData(0b0000_0001, 0b0000_0000, false, true, false, true)]
+    [InlineData(0b0000_0000, 0b1000_0000, true, false, true, false)]
+    [InlineData(0b0000_0001, 0b1000_0000,true, false, true, true)]
+    [InlineData(0b1111_1111, 0b1111_1111, true, false, true, true)]
+    [InlineData(0b1111_1111, 0b0111_1111, false, false, false, true)]
+    public void ROR_Executes_Correctly(byte input, byte result, bool negative, bool zero, bool carryOld, bool carry)
+    {
+        system.CPU.Data = input;
+        system.CPU.Registers.P.Carry = carryOld;
+        system.CPU.Address.Full = Address;
+        Machinery.Instructions.ReadModifyWrite.Operations.ROR(system.CPU);
+        
+        Assert.Equal(Address, system.RAM.LastWriteAddress);
+        Assert.Equal(result, system.RAM.PeekAt(Address));
+        CheckFlags(negative, zero, carry, interrupt: false, @decimal: false, overflow: false);
+    }
+}
--- CPU.MOS6502/Machinery/Instructions/ReadModifyWrite/Operations.cs
+++ CPU.MOS6502/Machinery/Instructions/ReadModifyWrite/Operations.cs
@@ -2,4 +2,59 @@ namespace CPU.MOS6502.Machinery.Instructions.ReadModifyWrite;
 
 static class Operations
 {
+    public static void ASL(Core cpu)
+    {
+        cpu.Registers.P.Carry = (cpu.Data & 0x80) != 0;
+        cpu.Data <<= 1;
+        cpu.Registers.P.Negative = (cpu.Data & 0x80) != 0;
+        cpu.Registers.P.Zero = cpu.Data == 0;
+        cpu.Bus.Write(cpu.Address, cpu.Data);
+    }
+    
+    public static void DEC(Core cpu)
+    {
+        cpu.Data -= 1;
+        cpu.Registers.P.Negative = (cpu.Data & 0x80) != 0;
+        cpu.Registers.P.Zero = cpu.Data == 0;
+        cpu.Bus.Write(cpu.Address, cpu.Data);
+    }
+    
+    public static void INC(Core cpu)
+    {
+        cpu.Data += 1;
+        cpu.Registers.P.Negative = (cpu.Data & 0x80) != 0;
+        cpu.Registers.P.Zero = cpu.Data == 0;
+        cpu.Bus.Write(cpu.Address, cpu.Data);
+    }
+    
+    public static void LSR(Core cpu)
+    {
+        cpu.Registers.P.Carry = (cpu.Data & 0x01) != 0;
+        cpu.Data >>>= 1;
+        cpu.Registers.P.Negative = false;
+        cpu.Registers.P.Zero = cpu.Data == 0;
+        cpu.Bus.Write(cpu.Address, cpu.Data);
+    }
+    
+    public static void ROL(Core cpu)
+    {
+        var carry = (byte)(cpu.Registers.P.Carry ? 1 : 0);
+        cpu.Registers.P.Carry = (cpu.Data & 0x80) != 0;
+        cpu.Data <<= 1;
+        cpu.Data |= carry;
+        cpu.Registers.P.Negative = (cpu.Data & 0x80) != 0;
+        cpu.Registers.P.Zero = cpu.Data == 0;
+        cpu.Bus.Write(cpu.Address, cpu.Data);
+    }
+    
+    public static void ROR(Core cpu)
+    {
+        var carry = (byte)(cpu.Registers.P.Carry ? 0x80 : 0);
+        cpu.Registers.P.Carry = (cpu.Data & 0x01) != 0;
+        cpu.Data >>>= 1;
+        cpu.Data |= carry;
+        cpu.Registers.P.Negative = (cpu.Data & 0x80) != 0;
+        cpu.Registers.P.Zero = cpu.Data == 0;
+        cpu.Bus.Write(cpu.Address, cpu.Data);
+    }
 }
