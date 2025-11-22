a121be7929275f5418c53bd058dac6210a00c093
rczy <berczy.balint@gmail.com>
2025-11-22 23:32:15 +0100

    Implemented branching and stack operations.

---
+++ CPU.MOS6502.Tests/Unit/Instructions/Operations/FlowAndStackTests.cs
@@ -0,0 +1,200 @@
+namespace CPU.MOS6502.Tests.Unit.Instructions.Operations;
+
+public class FlowAndStackTests : Base
+{
+    [Theory]
+    [InlineData(false, 1)]
+    [InlineData(true, 0)]
+    public void BCC_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Carry = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BCC(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: false, zero: false, carry: flag, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(false, 0)]
+    [InlineData(true, 1)]
+    public void BCS_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Carry = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BCS(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: false, zero: false, carry: flag, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(false, 0)]
+    [InlineData(true, 1)]
+    public void BEQ_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Zero = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BEQ(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: false, zero: flag, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(false, 0)]
+    [InlineData(true, 1)]
+    public void BMI_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Negative = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BMI(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: flag, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(false, 1)]
+    [InlineData(true, 0)]
+    public void BNE_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Zero = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BNE(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: false, zero: flag, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(false, 1)]
+    [InlineData(true, 0)]
+    public void BPL_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Negative = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BPL(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: flag, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void BRK_Executes_Correctly()
+    {
+        const ushort address = 0xABCD;
+        system.CPU.Address.Full = address;
+        Machinery.Instructions.FlowAndStack.Operations.BRK(system.CPU);
+        
+        Assert.Equal(address, system.CPU.Registers.PC);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Theory]
+    [InlineData(false, 1)]
+    [InlineData(true, 0)]
+    public void BVC_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Overflow = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BVC(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: flag);
+    }
+    
+    [Theory]
+    [InlineData(false, 0)]
+    [InlineData(true, 1)]
+    public void BVS_Executes_Correctly(bool flag, byte taken)
+    {
+        system.CPU.Registers.P.Overflow = flag;
+        Machinery.Instructions.FlowAndStack.Operations.BVS(system.CPU);
+        
+        Assert.Equal(taken, system.CPU.Data);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: flag);
+    }
+    
+    [Fact]
+    public void JMP_Executes_Correctly()
+    {
+        const ushort address = 0xABCD;
+        system.CPU.Address.Full = address;
+        Machinery.Instructions.FlowAndStack.Operations.JMP(system.CPU);
+        
+        Assert.Equal(address, system.CPU.Registers.PC);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void JSR_Executes_Correctly()
+    {
+        const ushort address = 0xABCD;
+        system.CPU.Address.Full = address;
+        Machinery.Instructions.FlowAndStack.Operations.JSR(system.CPU);
+        
+        Assert.Equal(address, system.CPU.Registers.PC);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void PHA_Executes_Correctly()
+    {
+        const byte data = 0xFF;
+        system.CPU.Registers.A = data;
+        Machinery.Instructions.FlowAndStack.Operations.PHA(system.CPU);
+        
+        Assert.Equal(data, system.CPU.Data);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void PHP_Executes_Correctly()
+    {
+        system.CPU.Registers.P = 0x00;
+        Machinery.Instructions.FlowAndStack.Operations.PHP(system.CPU);
+        
+        Assert.Equal(0x30, system.CPU.Data);
+        Assert.True(system.CPU.Registers.P.Break);
+        Assert.True(system.CPU.Registers.P.Unused);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void PLA_Executes_Correctly()
+    {
+        const byte data = 0xFF;
+        system.CPU.Data = data;
+        Machinery.Instructions.FlowAndStack.Operations.PLA(system.CPU);
+        
+        Assert.Equal(data, system.CPU.Registers.A);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void PLP_Executes_Correctly()
+    {
+        const byte data = 0xFF;
+        system.CPU.Data = data;
+        Machinery.Instructions.FlowAndStack.Operations.PLP(system.CPU);
+        
+        Assert.Equal(0xEF, (byte)system.CPU.Registers.P);
+        CheckFlags(negative: true, zero: true, carry: true, interrupt: true, @decimal: true, overflow: true);
+    }
+    
+    [Fact]
+    public void RTI_Executes_Correctly()
+    {
+        const ushort pc = 0x0200;
+        system.CPU.Registers.PC = pc;
+        Machinery.Instructions.FlowAndStack.Operations.RTI(system.CPU);
+        
+        Assert.Equal(pc, system.CPU.Registers.PC);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+    
+    [Fact]
+    public void RTS_Executes_Correctly()
+    {
+        const ushort pc = 0x0200;
+        system.CPU.Registers.PC = pc;
+        Machinery.Instructions.FlowAndStack.Operations.RTS(system.CPU);
+        
+        Assert.Equal(pc + 1, system.CPU.Registers.PC);
+        CheckFlags(negative: false, zero: false, carry: false, interrupt: false, @decimal: false, overflow: false);
+    }
+}
--- CPU.MOS6502/Machinery/Instructions/FlowAndStack/Operations.cs
+++ CPU.MOS6502/Machinery/Instructions/FlowAndStack/Operations.cs
@@ -2,27 +2,90 @@ namespace CPU.MOS6502.Machinery.Instructions.FlowAndStack;
 
 static class Operations
 {
-    public static void JSR(Core cpu)
+    public static void BCC(Core cpu)
+    {
+        cpu.Data = (byte)(!cpu.Registers.P.Carry ? 1 : 0);
+    }
+    
+    public static void BCS(Core cpu)
+    {
+        cpu.Data = (byte)(cpu.Registers.P.Carry ? 1 : 0);
+    }
+    
+    public static void BEQ(Core cpu)
+    {
+        cpu.Data = (byte)(cpu.Registers.P.Zero ? 1 : 0);
+    }
+    
+    public static void BMI(Core cpu)
+    {
+        cpu.Data = (byte)(cpu.Registers.P.Negative ? 1 : 0);
+    }
+    
+    public static void BNE(Core cpu)
+    {
+        cpu.Data = (byte)(!cpu.Registers.P.Zero ? 1 : 0);
+    }
+    
+    public static void BPL(Core cpu)
+    {
+        cpu.Data = (byte)(!cpu.Registers.P.Negative ? 1 : 0);
+    }
+    
+    public static void BRK(Core cpu)
     {
         cpu.Registers.PC = cpu.Address;
     }
-
-    public static void RTS(Core cpu)
+    
+    public static void BVC(Core cpu)
     {
-        cpu.Registers.PC++;
+        cpu.Data = (byte)(!cpu.Registers.P.Overflow ? 1 : 0);
     }
-
+    
+    public static void BVS(Core cpu)
+    {
+        cpu.Data = (byte)(cpu.Registers.P.Overflow ? 1 : 0);
+    }
+    
     public static void JMP(Core cpu)
     {
         cpu.Registers.PC = cpu.Address;
     }
 
-    public static void BRK(Core cpu)
+    public static void JSR(Core cpu)
     {
         cpu.Registers.PC = cpu.Address;
     }
-
+    
+    public static void PHA(Core cpu)
+    {
+        cpu.Data = cpu.Registers.A;
+    }
+    
+    public static void PHP(Core cpu)
+    {
+        cpu.Registers.P.Break = true;
+        cpu.Data = (byte)cpu.Registers.P;
+    }
+    
+    public static void PLA(Core cpu)
+    {
+        cpu.Registers.A = cpu.Data;
+    }
+    
+    public static void PLP(Core cpu)
+    {
+        var breakFlag = cpu.Registers.P.Break; 
+        cpu.Registers.P = cpu.Data;
+        cpu.Registers.P.Break = breakFlag;
+    }
+    
     public static void RTI(Core cpu)
     {
+    }
+
+    public static void RTS(Core cpu)
+    {
+        cpu.Registers.PC++;
     }
 }
