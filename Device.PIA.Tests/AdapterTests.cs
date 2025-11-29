2c52b80985bdec98de2f7fa7b4c21fdb7b416e84
rczy <berczy.balint@gmail.com>
2025-11-29 22:04:47 +0100

    Added projects for PIA (Peripheral Interface Adapter) implementation and tests. Changed CPU.MOS6502 from a console app to a class library.

---
+++ Device.PIA.Tests/AdapterTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Device.PIA.Tests;
+
+[Trait("Category", "Unit")]
+public class AdapterTests
+{
+    [Fact]
+    public void SampleTest_ShouldRun_Successfully()
+    {
+        Adapter pia = new();
+        Assert.Throws<NotImplementedException>(() => pia.Reset());
+        Assert.Throws<NotImplementedException>(() => pia.Read(0x0000));
+        Assert.Throws<NotImplementedException>(() => pia.Write(0x0000, 0xAB));
+    }
+}
---
+++ Device.PIA/Adapter.cs
@@ -0,0 +1,21 @@
+﻿namespace Device.PIA;
+
+using CPU.MOS6502;
+
+public class Adapter : IDevice
+{
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte Read(ushort address)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Write(ushort address, byte data)
+    {
+        throw new NotImplementedException();
+    }
+}
